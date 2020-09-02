316b621719303735fb8f819e857d37da7912059d
JavierHM-Audioslave <javorengero2000@yahoo.com.ar>
2020-09-02 17:29:38 -0300

    Agregado de listener a través de atributo onclick. Comentado el listener del elemento a través de Delegation. Ambas funcionan bien.

--- js/app.js
+++ js/app.js
@@ -53,6 +53,7 @@ function crearLiAYAdjuntar (tweet){
         eliminarTweet.classList.add("eliminarUnTweet");
         eliminarTweet.innerHTML = "X";
         eliminarTweet.setAttribute("href", "#");
+        eliminarTweet.onclick = borrarTweet; // Si voy a usar la función que está comentada 15 líneas más abajo, este línea simplemente la tengo que comentar. //
 
         let liNuevo = document.createElement("li");
         liNuevo.classList.add("liConTweet");
@@ -68,6 +69,8 @@ function crearLiAYAdjuntar (tweet){
 /* Feature: borrar tweet seleccionado del historial */
 /* ********************************************************************** */
 
+// Esta forma de usar el listener sobre un elemento padre, tengo entendido que se llama "Delegation". No la dejé de usar por ser más conveniente la otra forma (elemento.onclick), sino porque ésta fue la primera forma que se me ocurrió y la otra forma se me ocurrió días después. Dejo ambas para dejar registro que se puede hacer de ambas maneras y obtener el mismo resultado. //
+/* 
 let ulSeleccionado = document.querySelector("#tablaHistoricoDeTweets");
 ulSeleccionado.addEventListener("click", e => { // Acá lo hice con FUNCIÓN FLECHA a diferencia del otro eventListener de arriba. //
 
@@ -80,6 +83,20 @@ ulSeleccionado.addEventListener("click", e => { // Acá lo hice con FUNCIÓN FLE
         e.target.parentElement.remove();    // Por último, elimino el elemento "li" que contiene el tweet que el usuario está queriendo eliminar. //
     }
 });
+*/
+
+// Forma con método de agregarle un listener a un elemento. Ésto se ejecuta al hacer click en la cruz que sigue a un tweet en el historial. El agregado de esta función cuando se hace click en el elemento está en la línea 56. //
+function borrarTweet(e) {
+    let contenido = e.target.previousSibling.textContent; // Contenido del tweet que el usuario está eliminando. //
+        let tweets = JSON.parse(localStorage.getItem("Contenido 0"));
+        const indexDeTweetABorrarEnLS = tweets.indexOf(contenido); // Obtengo el índice del elemento del tweet que el usuario está eliminando. //
+        tweets.splice(indexDeTweetABorrarEnLS, 1);  // Borro del array el elemento que representa al tweet que el usuario está eliminando. //
+        localStorage.setItem("Contenido 0", JSON.stringify(tweets));    // Guardo en el "Contenido 0" del Local Storage el array actualizado sin el elemento que el usuario está queriendo eliminar. //
+        e.target.parentElement.remove();
+}
+
+
+
 
 /* ********************************************************************** */
 
